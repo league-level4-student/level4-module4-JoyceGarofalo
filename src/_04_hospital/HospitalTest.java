6dfdeaafb1745a5c4070349c3ce17c9d55c5bfbc
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2020-08-02 17:27:17 -0700

    hospital ip

--- src/_04_hospital/HospitalTest.java
+++ src/_04_hospital/HospitalTest.java
@@ -97,9 +97,18 @@ assertTrue(testDoctor.getPatients().size() == 3);
 
 	public void test8Patients() throws Exception {
 		// TODO: add 3 doctors to hospital
-
+		testHospital.addDoctor(generalPractitioner);
+		testHospital.addDoctor(surgeon); 
+		testHospital.addDoctor(generalPractitioner);
 		// TODO: add 8 patients to hospital
-
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
+		testHospital.addPatient(patient);
 		// hospital assigns patients to doctors
 		testHospital.assignPatientsToDoctors();
 		// hospital.getDoctors shows doctors have 3, 3, 2 patients
